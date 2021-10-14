39dc07f5a8fa81564e36c9ac2ac41652a6470247
Dmitry Polshakov <dmitry.polshakov@dsr-corporation.com>
2021-10-14 15:09:56 +0300

    feat: add USB trace in blinky_usb_logs example

--- examples/peripheral/blinky_usb_logs/main.c
+++ examples/peripheral/blinky_usb_logs/main.c
@@ -50,8 +50,133 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+
 #include "nrf_delay.h"
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
+
+#include "nrf_drv_usbd.h"
+#include "nrf_drv_clock.h"
+#include "app_usbd_core.h"
+#include "app_usbd.h"
+#include "app_usbd_string_desc.h"
+#include "app_usbd_cdc_acm.h"
+#include "app_usbd_serial_num.h"
+
 #include "boards.h"
+#include "app_timer.h"
+#include "app_button.h"
+
+static void log_init(void)
+{
+    ret_code_t err_code = NRF_LOG_INIT(NULL);
+    APP_ERROR_CHECK(err_code);
+
+    NRF_LOG_DEFAULT_BACKENDS_INIT();
+}
+
+static void usbd_user_ev_handler(app_usbd_event_type_t event)
+{
+    switch (event)
+    {
+    case APP_USBD_EVT_STOPPED:
+        app_usbd_disable();
+        break;
+
+    case APP_USBD_EVT_POWER_DETECTED:
+        if (!nrf_drv_usbd_is_enabled())
+        {
+            app_usbd_enable();
+        }
+        break;
+
+    case APP_USBD_EVT_POWER_REMOVED:
+        app_usbd_stop();
+        break;
+
+    case APP_USBD_EVT_POWER_READY:
+        app_usbd_start();
+        break;
+
+    default:
+        break;
+    }
+}
+
+#define LED_USB_RESUME      (BSP_BOARD_LED_0)
+#define LED_CDC_ACM_OPEN    (BSP_BOARD_LED_1)
+#define LED_CDC_ACM_RX      (BSP_BOARD_LED_2)
+#define LED_CDC_ACM_TX      (BSP_BOARD_LED_3)
+
+static void usbd_init(void)
+{
+    ret_code_t ret;
+
+    ret = nrf_drv_clock_init();
+    APP_ERROR_CHECK(ret);
+
+    nrf_drv_clock_lfclk_request(NULL);
+    while(!nrf_drv_clock_lfclk_is_running())
+    {
+        /* Just waiting */
+    }
+
+    app_usbd_serial_num_generate();
+
+    static const app_usbd_config_t usbd_config = {
+        .ev_state_proc = usbd_user_ev_handler
+    };
+    ret = app_usbd_init(&usbd_config);
+    APP_ERROR_CHECK(ret);
+}
+
+#define USBD_POWER_DETECTION true
+
+static void usbd_start(void)
+{
+    if (USBD_POWER_DETECTION)
+    {
+        ret_code_t ret;
+        ret = app_usbd_power_events_enable();
+        APP_ERROR_CHECK(ret);
+    }
+    else
+    {
+        NRF_LOG_INFO("No USB power detection enabled\r\nStarting USB now");
+
+        app_usbd_enable();
+        app_usbd_start();
+    }
+}
+
+#define CDC_ACM_COMM_INTERFACE  0
+#define CDC_ACM_COMM_EPIN       NRF_DRV_USBD_EPIN2
+
+#define CDC_ACM_DATA_INTERFACE  1
+#define CDC_ACM_DATA_EPIN       NRF_DRV_USBD_EPIN1
+#define CDC_ACM_DATA_EPOUT      NRF_DRV_USBD_EPOUT1
+
+/**
+ * @brief User event handler @ref app_usbd_cdc_acm_user_ev_handler_t (headphones)
+ * */
+static void cdc_acm_user_ev_handler(app_usbd_class_inst_t const * p_inst,
+                                    app_usbd_cdc_acm_user_event_t event)
+{
+}
+
+/**
+ * @brief CDC_ACM class instance
+ * */
+APP_USBD_CDC_ACM_GLOBAL_DEF(m_app_cdc_acm,
+                            cdc_acm_user_ev_handler,
+                            CDC_ACM_COMM_INTERFACE,
+                            CDC_ACM_DATA_INTERFACE,
+                            CDC_ACM_COMM_EPIN,
+                            CDC_ACM_DATA_EPIN,
+                            CDC_ACM_DATA_EPOUT,
+                            APP_USBD_CDC_COMM_PROTOCOL_AT_V250
+);
 
 /**
  * @brief Function for application main entry.
@@ -61,14 +186,24 @@ int main(void)
     /* Configure board. */
     bsp_board_init(BSP_INIT_LEDS);
 
+    usbd_init();
+
+    log_init();
+
+    usbd_start();
+
+    NRF_LOG_INFO("Start");
+
     /* Toggle LEDs. */
     while (true)
     {
-        for (int i = 0; i < LEDS_NUMBER; i++)
+        //NRF_LOG_INFO("Iter");
+        NRF_LOG_PROCESS();
+        while (app_usbd_event_queue_process())
         {
-            bsp_board_led_invert(i);
-            nrf_delay_ms(500);
+            /* Nothing to do */
         }
+
     }
 }
 
