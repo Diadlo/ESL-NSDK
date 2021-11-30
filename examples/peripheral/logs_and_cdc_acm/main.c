f6498073d53de998ac232cbd1a8b9927134bcb13
Dmitry Polshakov <dmitry.polshakov@dsr-corporation.com>
2021-11-30 12:16:28 +0300

    Add sample for logs and CDC ACM

---
+++ examples/peripheral/logs_and_cdc_acm/main.c
@@ -0,0 +1,183 @@
+/**
+ * Copyright 2021 Evgeniy Morozov
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE
+*/
+
+/** @file
+ *
+ * @defgroup logs_and_cdc_acm main.c
+ * @{
+ * @ingroup logs_and_cdc_acm
+ * @brief Example of using two ACMs simultaneously
+ *
+ * This example uses the first ACM for logging over USB and the second one
+ * for bidirectional communication with user.
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "nordic_common.h"
+#include "boards.h"
+
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
+
+#include "nrf_log_backend_usb.h"
+
+#include "app_usbd.h"
+#include "app_usbd_serial_num.h"
+#include "app_usbd_cdc_acm.h"
+
+
+#define READ_SIZE 1
+
+static char m_rx_buffer[READ_SIZE];
+
+static void usb_ev_handler(app_usbd_class_inst_t const * p_inst,
+                           app_usbd_cdc_acm_user_event_t event);
+
+/* Make sure that they don't intersect with LOG_BACKEND_USB_CDC_ACM */
+#define CDC_ACM_COMM_INTERFACE  2
+#define CDC_ACM_COMM_EPIN       NRF_DRV_USBD_EPIN3
+
+#define CDC_ACM_DATA_INTERFACE  3
+#define CDC_ACM_DATA_EPIN       NRF_DRV_USBD_EPIN4
+#define CDC_ACM_DATA_EPOUT      NRF_DRV_USBD_EPOUT4
+
+APP_USBD_CDC_ACM_GLOBAL_DEF(usb_cdc_acm,
+                            usb_ev_handler,
+                            CDC_ACM_COMM_INTERFACE,
+                            CDC_ACM_DATA_INTERFACE,
+                            CDC_ACM_COMM_EPIN,
+                            CDC_ACM_DATA_EPIN,
+                            CDC_ACM_DATA_EPOUT,
+                            APP_USBD_CDC_COMM_PROTOCOL_AT_V250);
+
+
+static void usb_ev_handler(app_usbd_class_inst_t const * p_inst,
+                           app_usbd_cdc_acm_user_event_t event)
+{
+    switch (event)
+    {
+    case APP_USBD_CDC_ACM_USER_EVT_PORT_OPEN:
+    {
+        ret_code_t ret;
+        bsp_board_led_on(0);
+        ret = app_usbd_cdc_acm_read(&usb_cdc_acm, m_rx_buffer, READ_SIZE);
+        UNUSED_VARIABLE(ret);
+        break;
+    }
+    case APP_USBD_CDC_ACM_USER_EVT_PORT_CLOSE:
+    {
+        bsp_board_led_off(0);
+        break;
+    }
+    case APP_USBD_CDC_ACM_USER_EVT_TX_DONE:
+    {
+        NRF_LOG_INFO("tx done");
+        bsp_board_led_invert(1);
+        break;
+    }
+    case APP_USBD_CDC_ACM_USER_EVT_RX_DONE:
+    {
+        bsp_board_led_invert(2);
+        ret_code_t ret;
+        do
+        {
+            /*Get amount of data transfered*/
+            size_t size = app_usbd_cdc_acm_rx_size(&usb_cdc_acm);
+            NRF_LOG_INFO("rx size: %d", size);
+
+            /* It's the simple version of an echo. Note that writing doesn't
+             * block execution, and if we have a lot of characters to read and
+             * write, some characters can be missed.
+             */
+            if (m_rx_buffer[0] == '\r' || m_rx_buffer[0] == '\n')
+            {
+                ret = app_usbd_cdc_acm_write(&usb_cdc_acm, "\r\n", 2);
+            }
+            else
+            {
+                ret = app_usbd_cdc_acm_write(&usb_cdc_acm,
+                                             m_rx_buffer,
+                                             READ_SIZE);
+            }
+
+            /* Fetch data until internal buffer is empty */
+            ret = app_usbd_cdc_acm_read(&usb_cdc_acm,
+                                        m_rx_buffer,
+                                        READ_SIZE);
+        } while (ret == NRF_SUCCESS);
+
+        break;
+    }
+    default:
+        break;
+    }
+}
+
+void logs_init()
+{
+    ret_code_t ret = NRF_LOG_INIT(NULL);
+    APP_ERROR_CHECK(ret);
+
+    NRF_LOG_DEFAULT_BACKENDS_INIT();
+}
+
+/**
+ * @brief Function for application main entry.
+ */
+int main(void)
+{
+    logs_init();
+
+    NRF_LOG_INFO("Starting up the test project with USB logging");
+
+    bsp_board_init(BSP_INIT_LEDS);
+
+    app_usbd_class_inst_t const * class_cdc_acm = app_usbd_cdc_acm_class_inst_get(&usb_cdc_acm);
+    ret_code_t ret = app_usbd_class_append(class_cdc_acm);
+    APP_ERROR_CHECK(ret);
+
+    while (true)
+    {
+        while (app_usbd_event_queue_process())
+        {
+            /* Nothing to do */
+        }
+
+        LOG_BACKEND_USB_PROCESS();
+        NRF_LOG_PROCESS();
+    }
+}
+
+/**
+ *@}
+ **/
